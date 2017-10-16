de0b8aa3c44a4bfae5d524c89ba4410b4eaa784f
ad5915 <dan.alex97@yahoo.com>
2017-10-16 22:27:44 +0100

    Added setup.py to be able to run tests.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name='attack-graphs',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+    test_suite='tests',
+    zip_safe=False,
+)
