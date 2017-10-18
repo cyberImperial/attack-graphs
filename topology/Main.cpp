325491001518b49b972e118b82ad183a6385f993
ad5915 <dan.alex97@yahoo.com>
2017-10-19 00:11:56 +0100

    Moved makefile to the root of the project and renamed the C++ module.

---
+++ topology/Main.cpp
@@ -0,0 +1,19 @@
+#include "Topology.hpp"
+#include "Host.hpp"
+
+using namespace std;
+
+/*nmap -oX traceroute_all_subnet.xml -O -sV localhost*/
+int main(int argc, char **argv)
+{
+  if(argc < 1)
+  {
+    cout << "Invalid number of program arguments: ";
+    cout << "Usage ./build_topology <traceroute_file.xml>" << endl;
+    exit(EXIT_FAILURE);
+  }
+  Host host(argv[1]);
+  host.load();
+  host.print_vulnerabilities();
+  return 0;
+}
