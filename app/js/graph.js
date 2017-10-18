527d506697b55f4e37423c82e19cceefd0a7ae98
Ioan Budea <ioanbudea@gmail.com>
2017-10-18 18:44:03 +0300

    Added an image for each node.

---
+++ app/js/graph.js
@@ -0,0 +1,93 @@
+let svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+let graph = {
+  "nodes": [
+    {"id": "ip1"},
+    {"id": "ip2"}
+  ],
+  "links": [
+    {"source": "ip1", "target": "ip2", "value": 1}
+  ]
+} 
+
+
+    
+let defs = svg.append("svg:defs");
+  
+defs.append("svg:pattern")
+    .attr("width", 1)
+    .attr("height", 1)
+    .attr("id", "server")
+    .append("svg:image")
+    .attr("xlink:href", "img/server.png")
+    .attr("width", 100)
+    .attr("height", 100)
+    .attr("x", 0)
+    .attr("y", 0);
+        
+let simulation = d3.forceSimulation()
+                .force("link", d3.forceLink().id(function(d) {
+                    return d.id;
+                }))
+                .force("charge", d3.forceManyBody().strength(-5))
+                .force("center", d3.forceCenter(width / 2, height / 2));
+    
+
+let link = svg.selectAll(".link")
+            .data(graph.links, function(d) {return d.target.id;})
+            .enter().append("line").attr("class", "link");
+            
+let node = svg.selectAll(".node")
+            .data(graph.nodes, function(d) {return d.id;})
+            .enter().append("g").attr("class", "node")
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
+      
+node.append("circle")
+    .attr("r", 50)
+    .style("fill", "url(#server)");
+
+node.append("title")
+    .text(function(d) { return d.id; });
+
+simulation.nodes(graph.nodes)
+    .on("tick", ticked);
+
+simulation.force("link")
+    .links(graph.links);
+
+function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("transform", function(d) {
+            return "translate(" + d.x + ", " + d.y + ")";
+        });
+  }
+
+function dragstarted(d) {
+    if (!d3.event.active)  {
+        simulation.alphaTarget(0.3).restart();
+    }
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragended(d) {
+    //if (!d3.event.active) {
+      //  simulation.alphaTarget(0);
+    //}
+    //d.fx = null;
+    //d.fy = null;
+}
