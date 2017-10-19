d9b0c701abe3ed074c8bcc1de0f540a9d57dea57
ad5915 <dan.alex97@yahoo.com>
2017-10-19 18:58:04 +0100

    Added the database.

---
+++ database/db_service.py
@@ -0,0 +1,20 @@
+import json
+import os
+from pprint import pprint
+
+class MemoryDB():
+    def __init__(self):
+        with open('database/indexed.idx', 'r') as index_file:
+            self.indexes = json.load(index_file)
+        with open('database/exports.idx', 'r') as export_file:
+            self.data = json.load(export_file)
+
+    def query(self, product, version):
+        index = str((product, version))
+        print(index)
+        for idx in self.indexes[index]:
+            output.append(self.data[idx])
+        return output
+
+DB = MemoryDB()
+DB.query(unicode("monkey_http_daemon"), unicode("0.7.0"))
