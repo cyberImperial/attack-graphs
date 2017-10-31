aec465bec46fae645aac4e87aaf94e7ae0940bad
Ioan Budea <ioanbudea@HUAWEI_P9_lite.localdomain>
2017-10-31 23:53:23 +0200

    Added functionality for obtaining privilege levels acquired using a certain vulnerability.

--- database/db_service.py
+++ database/db_service.py
@@ -20,3 +20,41 @@ class MemoryDB():
         for idx in self.indexes[index]:
             output.append(self.data[idx])
         return output
+
+    def get_privileges(self, product, version):
+        """ Returns a list of (CSV, admin)"""
+
+        def filter_by_field(data, field):
+            if not field in data:
+                return None
+            return data[field]
+
+        def parse_metric_v2(impact):
+            impact = filter_by_field(impact, "baseMetricV2")
+            if impact is None:
+                return None
+            if not "userInteractionRequired" in impact:
+                return None
+            if impact["userInteractionRequired"]:
+                return None
+            return {
+                "all" : impact["obtainAllPrivilege"],
+                "user" : impact["obtainUserPrivilege"],
+                "other" : impact["obtainOtherPrivilege"]
+            }
+
+        vulenerability_list = self.query(product, version)
+        levels = {
+            "all" : False,
+            "user" : False,
+            "other" : False
+        }
+        for vulnerability in vulenerability_list:
+            if "impact" in vulnerability:
+                v2_out = parse_metric_v2(vulnerability["impact"])
+                # Will add V3 only if needed
+
+                if not v2_out is None:
+                    for k in levels:
+                        levels[k] = levels[k] or v2_out[k]
+        return levels
