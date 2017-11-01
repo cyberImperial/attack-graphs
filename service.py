033dd42da9d2d2d1191138bb43d2cfcb701e301d
ad5915 <dan.alex97@yahoo.com>
2017-11-01 04:08:34 +0000

    Added absolute path to database/load.py and erased useless lines from service.py

--- service.py
+++ service.py
@@ -1,130 +1,13 @@
 import sys
 import threading
-import requests
-
-from networks.network import Network
-from networks.network import default_network
-from database.db_service import MemoryDB
 import os
-import json
+
 from pprint import pprint
 import subprocess
 import signal
-import traceback
 
-from flask import Flask, request
-
-app = Flask(__name__)
-
-def get_port():
-    port = 4242
-    try:
-        port = int(sys.argv[1])
-    except Exception as e:
-        pass
-    return port
-
-def run_server():
-    DB = MemoryDB()
-
-    @app.route('/', methods=['GET'])
-    def healthy():
-        if request.method == 'GET':
-            return "healthy"
-
-    @app.route("/vulnerability", methods=['POST'])
-    def query():
-        if request.method == 'POST':
-            req = request.get_json()
-            outputs = []
-            for entry in req:
-                outputs.append(DB.query(entry["product"], entry["version"]))
-            return str(outputs)
-
-    @app.route("/privileges", methods=['POST'])
-    def privileges():
-        if request.method == 'POST':
-            req = request.get_json()
-            outputs = {}
-            for entry in req:
-                key = str((entry["product"], entry["version"]))
-                outputs[key] = DB.get_privileges(entry["product"], entry["version"])
-            return str(outputs)
-
-    app.run(host='0.0.0.0', port=get_port())
-
-def cli():
-    default_request = json.loads("""[{
-        "product" : "monkey_http_daemon",
-        "version" : "0.7.0"
-    }, {
-        "product" : "qemu",
-        "version" : "0.13.0"
-    }]""")
-
-    while True:
-        sys.stdout.write('>')
-        sys.stdout.flush()
-
-        line = sys.stdin.readline()
-        if "print" in line:
-            for net in network.subnets:
-                print(network.inspect_subnet(net))
-        if "exit" in line:
-            exit(0)
-        if "delete" in line:
-            network.stop_network()
-        if "add node" in line:
-            network.add_node(Network.metasploitable, "")
-            network.connect_to_subnet(network.nodes[-1], network.subnets[0])
-        if "request" in line:
-            try:
-                r = requests.get("http://127.0.0.1:" + str(get_port()) + "/")
-                print(r.text)
-            except Exception as e:
-                print("Error: request failed.")
-                print(e)
-        if "query" in line:
-            r = requests.post(
-                url = "http://127.0.0.1:" + str(get_port()) + "/vulnerability",
-                json = default_request)
-            print(r.text)
-        if "privileges" in line:
-            r = requests.post(
-                url = "http://127.0.0.1:" + str(get_port()) + "/privileges",
-                json = default_request)
-            print(r.text)
-        if "discovery" in line:
-            try:
-                subnet_mask = line.split(" ")[1]
-            except Exception as e:
-                subnet_mask = "172.18.0.0/29"
-            out = subprocess.getoutput("nmap -sP " + subnet_mask)
-
-            ips = []
-            hosts = []
-            for word in out.split(" "):
-                if len(word.split(".")) == 4:
-                    ips.append(word.split("\n")[0])
-            print(ips)
-            for ip in ips:
-                default = {"Host" : ip }
-
-                # Getting host information for ip
-                try:
-                    subprocess.getoutput("nmap -oX host.xml -O -sV " + ip)
-                    subprocess.getoutput("nmap -oX trace.xml --traceroute " + ip)
-                    host_json = subprocess.getoutput("./build_topology host.xml trace.xml")
-                    if "Aborted" in host_json:
-                        hosts.append(default)
-                    else:
-                        hosts.append(json.loads(host_json))
-                except Exception as e:
-                    hosts.append(default)
-            with open("frontend_data.json", "w") as output:
-                 json.dump(hosts, output)
-            pprint(hosts)
-
+from service.database_server import database_server
+from service.cli import cli
 
 def signal_handler(signal, frame):
     os.system("docker rm -f $(docker ps -a -q)")
@@ -137,10 +20,6 @@ if __name__ == "__main__":
         print("Must be run as root.")
         exit(1)
     signal.signal(signal.SIGINT, signal_handler)
-    network = default_network()
 
-    # Starting a thread that runs the server
-    threading.Thread(target=run_server).start()
-
-    # Starting a thread that runs the command line interface
+    threading.Thread(target=database_server).start()
     threading.Thread(target=cli).start()
