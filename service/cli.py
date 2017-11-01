913d4442a0e26bf70c71c2cf2dfc2cb4db5877fb
ad5915 <dan.alex97@yahoo.com>
2017-11-01 03:40:36 +0000

    Added support for the database services in the cli.

--- service/cli.py
+++ service/cli.py
@@ -5,17 +5,46 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from networks.network import Network
 from networks.network import default_network
-import operator
+from service.server import config
+
+import json, requests
 
 class CLI():
     def __init__(self):
         def init_network():
             self.network = default_network()
 
+        def add_node():
+            self.network.add_node(Network.metasploitable, "")
+            self.network.connect_to_subnet(network.nodes[-1], network.subnets[0])
+
+        def request(line, component, url):
+            line = line.split("\n")[0]
+            args = len(line.split(" "))
+            if args != 3:
+                return
+            product, version = tuple(line.split(" ")[1:])
+            # e.g. qemu 0.13.0
+            request = json.loads("[{ \
+                \"product\" : \"" + product + "\",\
+                \"version\" : \"" + version + "\"\
+            }]")
+
+            try:
+                r = requests.post(
+                    url = "http://127.0.0.1:" + str(config[component]) + url,
+                    json = request)
+                print(r.text)
+            except Exception as e:
+                pass
+
         self.functions = {
             "echo" : lambda x: print(x[4:]),
             "start" : lambda x: init_network(),
-            "stop" : lambda x: self.network.stop_network()
+            "delete" : lambda x: self.network.stop_network(),
+            "add node" : lambda x: add_node(),
+            "query" : lambda x: request(x, "database", "vulnerability"),
+            "privileges" : lambda x: request(x, "database", "privileges")
         }
 
     def dispatch(self, line):
--- service/database_server.py
+++ service/database_server.py
@@ -11,30 +11,7 @@ def database_server():
     db = MemoryDB()
     server = Server("database", config["database"])
 
-    server.add_component_post("/test", DBQuery(db))
-    server.add_component_post("/test2", DBPrivileges(db))
+    server.add_component_post("/vulnerability", DBQuery(db))
+    server.add_component_post("/privileges", DBPrivileges(db))
 
     server.run()
-
-# import threading
-#
-# threading.Thread(target=database_server).start()
-#
-# time.sleep(10)
-# import json, requests
-#
-# default_request = json.loads("""[{
-#     "product" : "monkey_http_daemon",
-#     "version" : "0.7.0"
-# }, {
-#     "product" : "qemu",
-#     "version" : "0.13.0"
-# }]""")
-# r = requests.post(
-#     url = "http://127.0.0.1:" + str(config["database"]) + "/test",
-#     json = default_request)
-# print(r.text)
-# r = requests.post(
-#     url = "http://127.0.0.1:" + str(config["database"]) + "/test2",
-#     json = default_request)
-# print(r.text)
