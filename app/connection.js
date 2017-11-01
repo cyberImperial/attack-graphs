a3136e23edd103a862ad271874803714cc3b300c
ad5915 <dan.alex97@yahoo.com>
2017-11-01 17:32:57 +0000

    Reverted back to app.

--- app/connection.js
+++ app/connection.js
@@ -2,8 +2,13 @@ const jquery = require("jquery");
 const host = "http://127.0.0.1:4242";
 
 jquery.ajax({
-  type: "GET",
-  url: host,
+  type: "POST",
+  data: [{
+    "product" : "qemu",
+    "version" : "0.13.0"
+  }],
+  dataType: 'json',
+  url: host + "/privileges",
   success: function (data) {
     console.log(data);
   }
