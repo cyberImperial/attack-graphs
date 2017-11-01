3d92a75ea4a51ce1e70a6cd2a7a0033d25b73e5c
Ioan Budea <ioanbudea@gmail.com>
2017-11-01 22:41:49 +0200

    Fixed the example for obtaining vulnerability details by sending a request to the local server.

--- app/connection.js
+++ app/connection.js
@@ -1,15 +1,24 @@
-const jquery = require("jquery");
-const host = "http://127.0.0.1:4242";
+let request = require("request")
 
-jquery.ajax({
-  type: "POST",
-  data: [{
-    "product" : "qemu",
-    "version" : "0.13.0"
-  }],
-  dataType: 'json',
-  url: host + "/privileges",
-  success: function (data) {
-    console.log(data);
-  }
-});
+// JSON to be passed to the QPX Express API
+let requestData = [{
+    "product": "qemu",
+    "version": "0.13.0"
+}]
+
+let url = "http://127.0.0.1:4242"
+
+request({
+    url: url + "/privileges",
+    method: "POST",
+    json: requestData
+}, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body)
+        } else {
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusText)
+        }
+    }
+);
