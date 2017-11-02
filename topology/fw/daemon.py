3cf179009939422228657c7582a6440c42e6fc6e
ad5915 <dan.alex97@yahoo.com>
2017-11-02 15:11:59 +0000

    Finished sniffer daemon.

--- topology/fw/daemon.py
+++ topology/fw/daemon.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import os, sys, time
 from threading import Lock
 import threading
+import pcapy
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
@@ -11,10 +12,10 @@ from topology.fw.sniffer import parse_packet
 from service.components import Component
 from service.server import Server, config
 
-def do_post():
+def do_get():
     full_url = "http://127.0.0.1:30001/newpackets"
     try:
-        print("here")
+        print("Doing test request...")
         r = requests.get(full_url)
         print(r.text)
     except Exception as e:
@@ -22,13 +23,42 @@ def do_post():
 
 def test_req():
     while True:
-        do_post()
-        time.sleep(2)
+        do_get()
+        time.sleep(5)
 
 class Sniffer():
-    def run():
+    def __init__(self, shared_packets, lock):
+        self.packets = shared_packets
+        self.lock = lock
+
+    def run(self):
+        devices = pcapy.findalldevs()
+        print("Starting to listen on decived:" + str(devices))
+
+        # connections = []
+        # for dev in devices:
+        #     try:
+        #         connection = pcapy.open_live(dev , 65536 , True , 0)
+        #         connections.append(connection)
+        #     except Exception as e:
+        #         print("Device " + dev + " failed.")
+
+        connections = [pcapy.open_live("wlp8s0" , 65536 , True , 0)]
+
         while True:
-            pass
+            new_packets = []
+            for cap in connections:
+                (header, packet) = cap.next()
+                packet = parse_packet(packet)
+                if packet is not None:
+                    new_packets.append(packet)
+
+            self.lock.acquire()
+
+            for packet in new_packets:
+                self.packets.append(packet)
+
+            self.lock.release()
 
 class SniffingDaemon(Component):
     def __init__(self, shared_packets, lock):
@@ -36,23 +66,25 @@ class SniffingDaemon(Component):
         self.lock = lock
 
     def process(self, unused):
-        print("arrived")
         self.lock.acquire()
 
-        packets = self.shared_packets
-        self.shared_packets = []
+        packets = self.shared_packets[:]
+        self.shared_packets[:] = []
 
         self.lock.release()
         return packets
 
 def run_server():
-    server = Server("sniffer", 30001)
-    print(server.add_component_post)
-    print(server.add_component_get)
+    shared_lock = Lock()
+    shared_list = []
 
-    server.add_component_get("/newpackets", SniffingDaemon(["dani mocanu"], Lock()))
+    server = Server("sniffer", 30001)
+    server.add_component_get("/newpackets", SniffingDaemon(shared_list, shared_lock))
+
+    sniffer = Sniffer(shared_list, shared_lock)
 
     threading.Thread(target=server.run).start()
+    threading.Thread(target=sniffer.run).start()
     threading.Thread(target=test_req).start()
 
 if __name__ == "__main__":
