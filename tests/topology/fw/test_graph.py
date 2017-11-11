0c00343f6753aa15795c564e5241af914ab0190a
Adi Catana <adrian.catana15@imperial.ac.uk>
2017-11-11 20:04:46 +0000

    Moved graph testing in tests/it runs now with the other tests

--- tests/topology/fw/test_graph.py
+++ tests/topology/fw/test_graph.py
@@ -1,7 +1,7 @@
-import unittest
+from __future__ import absolute_import
 from unittest import TestCase
-from graph import Node
-from graph import Graph
+from topology.fw.graph import Node
+from topology.fw.graph import Graph
 
 class TestNode(TestCase):
     def setUp(self):
@@ -15,7 +15,7 @@ class TestGraph(TestCase):
     def setUp(self):
         self.graph = Graph()
 
-    def add_edge_test(self):
+    def test_add_edge(self):
         self.node1 = Node("192.82.21.5")
         self.node2 = Node("192.82.21.6")
 
@@ -24,8 +24,8 @@ class TestGraph(TestCase):
         self.assertEqual(len(self.graph.nodes), 2)
         self.assertTrue((self.node1, self.node2) in self.graph.edges)
 
-    def add_duplicate_node(self):
-        self.add_edge_test()
+    def test_add_duplicate_node(self):
+        self.test_add_edge()
 
         self.node1 = Node("192.82.21.5")
         self.node2 = Node("190.80.20.0")
@@ -33,20 +33,8 @@ class TestGraph(TestCase):
 
         self.assertEqual(len(self.graph.nodes), 3)
 
-    def add_duplicate_edge(self):
-        self.add_edge_test()
-        self.add_edge_test()
+    def test_add_duplicate_edge(self):
+        self.test_add_edge()
+        self.test_add_edge()
         self.assertEqual(len(self.graph.edges), 1)
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestNode('test_get_ip_simple'))
-    suite.addTest(TestGraph('add_edge_test'))
-    suite.addTest(TestGraph('add_duplicate_node'))
-    suite.addTest(TestGraph('add_duplicate_edge'))        
-    return suite
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
 
