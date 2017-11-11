1d69b28a1fb0af93a570a58eb83ca2ac05370205
mz4715@ic.ac.uk <mz4715@ic.ac.uk>
2017-11-11 22:47:02 +0000

    Added tests for populator

---
+++ tests/topology/fw/test_populator.py
@@ -0,0 +1,147 @@
+from __future__ import absolute_import
+import unittest
+from unittest import TestCase
+from topology.fw.populator import Populator
+from database.db_service import MemoryDB
+from topology.fw.graph import Graph
+from topology.fw.graph import Node
+import json
+
+class TestPopulator(TestCase):
+    def setUp(self):
+        self.graph = Graph()
+        self.graph.add_edge(Node("190.80.50.0"), Node("190.80.50.1"))
+        self.graph.add_edge(Node("190.80.50.1"), Node("190.80.50.2"))
+
+        self.json1 = {
+           "Host":{
+              "os":"ASUS RT-N56U WAP (Linux 3.4)",
+              "ip":"192.30.252.154",
+              "RunningServices":[
+                 {
+                    "Port":{
+                       "portid":"443",
+                       "protocol":"tcp"
+                    },
+                    "Service":{
+                       "name":"https",
+                       "product":"GitHub.com",
+                       "version":"attributeMissing",
+                       "state_open":"open",
+                       "reason":"syn-ack"
+                    }
+                 },
+                 {
+                    "Port":{
+                       "portid":"80",
+                       "protocol":"tcp"
+                    },
+                    "Service":{
+                       "name":"http",
+                       "product":"GitHub.com",
+                       "version":"attributeMissing",
+                       "state_open":"open",
+                       "reason":"syn-ack"
+                    }
+                 }
+              ]
+           }
+        }
+
+        self.json2 = {
+           "Host":{
+              "os":"ASUS ROG WAP (Linux 3.4)",
+              "ip":"192.30.250.155",
+              "RunningServices":[
+                 {
+                    "Port":{
+                       "portid":"443",
+                       "protocol":"tcp"
+                    },
+                    "Service":{
+                       "name":"Demo",
+                       "product":"GitHub.com",
+                       "version":"3.1",
+                       "state_open":"open",
+                       "reason":"syn-ack"
+                    }
+                 },
+                 {
+                    "Port":{
+                       "portid":"80",
+                       "protocol":"tcp"
+                    },
+                    "Service":{
+                       "name":"Demo2",
+                       "product":"GitHub.com",
+                       "version":"3.0",
+                       "state_open":"open",
+                       "reason":"syn-ack"
+                    }
+                 }
+              ]
+           }
+        }
+
+        #
+        # def mock_discovery_ip(ip):
+        #     return {
+        #         "test" : "test"
+        #     }
+        #
+        # def mock_db_request(line, component, url):
+        #     return {
+        #         "Vulnerability" :
+        #     }
+        #
+        # self.Populator = Populator(graph, mock_discovery_ip, mock_db_request)
+
+    def test_get_batch(self):
+        populator = Populator(self.graph)
+        populator.threads = 3
+
+        i1, i2, mybatch = populator.get_batch(self.graph)
+
+        self.assertEqual(mybatch[0], '190.80.50.0')
+        self.assertEqual(mybatch[1], "190.80.50.1")
+        self.assertEqual(mybatch[2], "190.80.50.2")
+
+    def test_get_ips(self):
+        def discovery_ip2(ip):
+            return {
+                "ip" : ip
+            }
+
+        populator = Populator(self.graph, discovery_ip2)
+        results = populator.get_ips(["190.80.50.0", "190.80.50.1"])
+
+        self.assertDictEqual(results[0], {"ip" : "190.80.50.0"})
+        self.assertDictEqual(results[1], {"ip" : "190.80.50.1"})
+
+    def test_good_formatted_vulnerabilities_are_added(self):
+
+        def db_request(query, unused1, unused2):
+            if "query" in query:
+                return {"vul" : "vul"}
+            if "privileges" in query:
+                return {"pri" : "pri"}
+            return None
+
+        populator = Populator(self.graph, db_request=db_request)
+
+        init_results = [self.json1, self.json2]
+        final_results = populator.add_vulnerabilities(init_results)
+
+        for result in final_results:
+            self.assertDictEqual(result["Host"]["RunningServices"][0]["Vulnerability"], {"vul" : "vul"})
+            self.assertDictEqual(result["Host"]["RunningServices"][0]["Privileges"], {"pri" : "pri"})
+
+    def test_update_graph(self):
+
+        populator = Populator(self.graph)
+        res = [self.json1, self.json2]
+
+        populator.update_graph(self.graph, res, 0, 2)
+
+        for i in range(0, 2):
+            self.assertDictEqual(self.graph.nodes[i].running, res[i])
