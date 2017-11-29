f05b41a8c84bfcda6fcebbafce10a3989ed10311
ad5915 <dan.alex97@yahoo.com>
2017-11-29 19:30:02 +0000

    Added system handling for closing the application.

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 import sys
 import threading
 import os
+import signal
 
 from topology.graph.graph_service import graph_service
 from topology.sniffer.sniffing_service import sniffing_service
@@ -8,17 +9,36 @@ from database.database_service import database_service
 
 from multiprocessing import Process
 
+def signal_handler(siganl, frames):
+    print("Killing the running services.")
+    for process in processes:
+        print("Killing process {}".format(process.pid))
+        os.system("kill -9 {}".format(process.pid))
+    sys.exit(0)
+
 def services():
-    Process(target=database_service).start()
-    Process(target=sniffing_service).start()
-    Process(target=graph_service).start()
+    global processes
+    processes = [
+        Process(target=database_service),
+        Process(target=sniffing_service),
+        Process(target=graph_service)
+    ]
+
+    for process in processes:
+        process.start()
 
 if __name__ == "__main__":
     if os.getuid() != 0:
         print("Must be run as root.")
         exit(1)
 
+    if len(sys.argv) < 2:
+        print("Need to specify master or slave.")
+        exit(1)
+
     services()
+    signal.signal(signal.SIGINT, signal_handler)
+
     if sys.argv[1] == "master":
         os.system("python3 dissemination/master.py")
 
