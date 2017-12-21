59857dcd3de328ff60c393ab7f76f7d4e898c4d8
ad5915 <dan.alex97@yahoo.com>
2017-12-21 07:36:13 +0200

    Added a connection simulation and added support for dictionaries in parse_packet for simplicity

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -5,9 +5,12 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from topology.graph.graph import Graph
+from topology.graph.graph import Node
 
 import json
 import ast
+
+from random import randrange
 
 class Simulation():
     def __init__(self, conf_file):
@@ -20,10 +23,39 @@ class Simulation():
         self.graph = Graph.from_json(self.conf)
         print("Graph successfully loaded...")
 
-    def get_packet(self):
-        pass
+    def connection(self):
+        def build_packet(src, dest):
+            return {
+                "src" : str(src),
+                "dest" : str(dest)
+            }
 
-    def discovery_ip(self):
-        pass
+        class Connection():
+            def __init__(self, graph):
+                self.graph = graph
+
+            def next(self):
+                # return a new random packet sent between 2 nodes of the graph
+                link_idx = randrange(len(self.graph.edges))
+                for (n1, n2) in self.graph.edges:
+                    if link_idx == 0:
+                        return build_packet(n1.ip, n2.ip)
+                    link_idx -= 1
+
+                raise Exception("Malformed simulation graph!")
+
+        return Connection(self.graph)
+
+    def discovery_ip(self, ip):
+        for node in self.graph.nodes:
+            if Node(ip) == node:
+                return node.running
+        return {}
 
 simulation = Simulation("simple.json")
+print(simulation.discovery_ip("10.1.3.1"))
+connection = simulation.connection()
+packets = 10
+while packets > 0:
+    print(connection.next())
+    packets -= 1
