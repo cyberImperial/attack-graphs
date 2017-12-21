0a9b494e71f42c41022a0a32e1a89776f5adfc10
ad5915 <dan.alex97@yahoo.com>
2017-12-21 20:16:43 +0200

    Elimineted subnets and added some style changes.

--- inference/mulval_translator.py
+++ inference/mulval_translator.py
@@ -11,27 +11,25 @@ from inference.mock_data import mock_data
 
 class MulvalTranslator():
     def __init__(self):
-        self.f = open('mulval_input.P', 'w')
-        self.s = set()
+        self.mulval_file = open('mulval_input.P', 'w')
+        self.active_set = set()
 
-    def makeTopology(self):
-        self.f.write("attackerLocated(internet).\n")
-        self.f.write("attackGoal(execCode(_, _)).\n")
-        self.f.write("hacl(X, Y, _, _) :-\n "
-                     + " inSubnet(X,S),\n"
-                     + "  inSubnet(Y,S).\n\n" )
+    def make_topology(self):
+        self.mulval_file.write("attackerLocated(internet).\n")
+        self.mulval_file.write("attackGoal(execCode(_, _)).\n")
 
-        self.f.write("hacl(internet, '" + self.data["links"][0]["source"] +
-                     "', _, _).\n")
+        # Hardcoded...
+        self.mulval_file.write("hacl(internet, '{}', _, _).\n".format(self.data["links"][0]["source"]))
+
+        def add_edge(edge_from, edge_to):
+            if not (edge_from, edge_to) in self.active_set:
+                self.mulval_file.write("hacl('{}', '{}', _, _).\n".format(edge_from, edge_to))
+                self.active_set.add((edge_from, edge_to))
+
         for link in self.data["links"]:
-            if not link["source"] in self.s:
-                self.f.write(" inSubnet('" + link["source"] + "', subnet).\n")
-                self.s.add(link["source"])
-            if not link["target"] in self.s:
-                self.f.write(" inSubnet('" + link["target"] + "', subnet).\n")
-                self.s.add(link["target"])
+            add_edge(link["source"], link["target"])
 
-    def addVulnerabilities(self):
+    def add_vulnerabilities(self):
         for host in self.data["hosts"]:
             if host["running"]["scanned"] == "false":
                 continue
@@ -53,9 +51,9 @@ class MulvalTranslator():
                     if access == "LOCAL": access = "localExploit"
                     if access == "NETWORK": access = "remoteExploit"
 
-                    self.f.write("networkServiceInfo('%s', '%s', '%s', '%s', '%s').\n" % (ip, application, protocol, port, privileges))
-                    self.f.write("vulExists('%s', '%s', '%s').\n" % (ip, vulnerability, application))
-                    self.f.write("vulProperty('%s', %s, %s).\n" % (vulnerability, access, 'privEscalation'))
+                    self.mulval_file.write("networkServiceInfo('%s', '%s', '%s', '%s', '%s').\n" % (ip, application, protocol, port, privileges))
+                    self.mulval_file.write("vulExists('%s', '%s', '%s').\n" % (ip, vulnerability, application))
+                    self.mulval_file.write("vulProperty('%s', %s, %s).\n" % (vulnerability, access, 'privEscalation'))
 
 if __name__ == "__main__":
     mulval = None
@@ -67,9 +65,9 @@ if __name__ == "__main__":
     if mulval.data is None:
         mulval = mock_data(MulvalTranslator())
 
-    mulval.makeTopology()
-    mulval.addVulnerabilities()
-    mulval.f.close()
+    mulval.make_topology()
+    mulval.add_vulnerabilities()
+    mulval.mulval_file.close()
 
     os.system("export MULVALROOT=/home/ad5915/mulval")
     os.system("PATH=$PATH:$MULVALROOT/bin")
