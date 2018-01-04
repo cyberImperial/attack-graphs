60c6991211a92095a5800cc2b2e40d97dad7fa0a
Ioan Budea <ioanbudea@gmail.com>
2018-01-05 01:26:11 +0200

    Refresh button clears the graph and reloads it, added dummy data for the attack graph view on toggle.

--- app/js/graph.js
+++ app/js/graph.js
@@ -14,8 +14,13 @@ let svg = d3.select("#graph-container")
 
 let graph = {};
 let showTooltip = true;
+let reachabilityMode = true;
 
-function getTopologyInfo() {
+function getReachability() {
+    // remove all elements to redraw the graph on refresh
+    svg.selectAll("*").remove();
+
+    // get the new data
     jquery.ajax({
         type: "GET",
         url: host,
@@ -23,7 +28,7 @@ function getTopologyInfo() {
             data = JSON.parse(data.replace(/\'/g, "\""));
             // console.log(data.hosts);
             // console.log(data.links);
-            document.getElementById("devices").innerHTML = "Devices: " + data.hosts.length;
+            document.getElementById("devices").innerHTML = "Devices: " + (data.hosts.length - 1);
             document.getElementById("detectedLinks").innerHTML = "Active Links: " + data.links.length;
             let graph = {};
             graph.nodes = data.hosts;
@@ -59,7 +64,7 @@ function getTopologyInfo() {
 
                 .append("svg:image")
                 .attr("xlink:href", function(d) {
-                    console.log(d);
+                    // console.log(d);
                     if(d.ip !== "255.255.255.255") {
                         return "img/server.png";
                     } else {
@@ -85,7 +90,6 @@ function getTopologyInfo() {
                         var result = "";
                         total += d.running.Host.RunningServices.length;
                         for (var i = 0; i < d.running.Host.RunningServices.length; i++) {
-
 
                             // Usual display attributes
                             if (d.running.Host.RunningServices[i].Port.portid != "attributeMissing") {
@@ -187,6 +191,158 @@ function getTopologyInfo() {
             }
         }
     })
-};
+}
 
-getTopologyInfo();
+function getAttackGraph() {
+    // remove all elements to redraw the graph on refresh
+    svg.selectAll("*").remove();
+
+    // get the new data
+    var colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+    svg.append('defs').append('marker')
+        .attrs({'id':'arrowhead',
+            'viewBox':'-0 -5 10 10',
+            'refX':13,
+            'refY':0,
+            'orient':'auto',
+            'markerWidth':13,
+            'markerHeight':13,
+            'xoverflow':'visible'})
+        .append('svg:path')
+        .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+        .attr('fill', '#999')
+        .style('stroke','none');
+
+    var simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(1))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+    d3.json("js/graph.json", function (error, graph) {
+        if (error) throw error;
+        update(graph.links, graph.nodes);
+    })
+
+    function update(links, nodes) {
+        link = svg.selectAll(".link")
+            .data(links)
+            .enter()
+            .append("line")
+            .attr("class", "link")
+            .attr('marker-end','url(#arrowhead)')
+
+        link.append("title")
+            .text(function (d) {return d.type;});
+
+        edgepaths = svg.selectAll(".edgepath")
+            .data(links)
+            .enter()
+            .append('path')
+            .attrs({
+                'class': 'edgepath',
+                'fill-opacity': 0,
+                'stroke-opacity': 0,
+                'id': function (d, i) {return 'edgepath' + i}
+            })
+            .style("pointer-events", "none");
+
+        edgelabels = svg.selectAll(".edgelabel")
+            .data(links)
+            .enter()
+            .append('text')
+            .style("pointer-events", "none")
+            .attrs({
+                'class': 'edgelabel',
+                'id': function (d, i) {return 'edgelabel' + i},
+                'font-size': 10,
+                'fill': '#aaa'
+            });
+
+        edgelabels.append('textPath')
+            .attr('xlink:href', function (d, i) {return '#edgepath' + i})
+            .style("text-anchor", "middle")
+            .style("pointer-events", "none")
+            .attr("startOffset", "50%")
+            .text(function (d) {return d.type});
+
+        node = svg.selectAll(".node")
+            .data(nodes)
+            .enter()
+            .append("g")
+            .attr("class", "node")
+            .call(d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                //.on("end", dragended)
+            );
+
+        node.append("circle")
+            .attr("r", 5)
+            .style("fill", function (d, i) {return colors(i);})
+
+        node.append("title")
+            .text(function (d) {return d.id;});
+
+        node.append("text")
+            .attr("dy", -3)
+            .text(function (d) {return d.name+":"+d.label;});
+
+        simulation
+            .nodes(nodes)
+            .on("tick", ticked);
+
+        simulation.force("link")
+            .links(links);
+    }
+
+    function ticked() {
+        link
+            .attr("x1", function (d) {return d.source.x;})
+            .attr("y1", function (d) {return d.source.y;})
+            .attr("x2", function (d) {return d.target.x;})
+            .attr("y2", function (d) {return d.target.y;});
+
+        node
+            .attr("transform", function (d) {return "translate(" + d.x + ", " + d.y + ")";});
+
+        edgepaths.attr('d', function (d) {
+            return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
+        });
+
+        edgelabels.attr('transform', function (d) {
+            if (d.target.x < d.source.x) {
+                var bbox = this.getBBox();
+
+                rx = bbox.x + bbox.width / 2;
+                ry = bbox.y + bbox.height / 2;
+                return 'rotate(180 ' + rx + ' ' + ry + ')';
+            }
+            else {
+                return 'rotate(0)';
+            }
+        });
+    }
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+    }
+    
+}
+
+function getGraphInfo() {
+    if(reachabilityMode) {
+        getReachability();
+    } else {
+        getAttackGraph();
+    }
+}
+
+getGraphInfo();
